742e8531627c8fcdb15a677d5a985112b8cf0d35
Kamila Kulczyk <kamila.kulczyk@gmail.com>
2023-10-20 11:00:21 +0200

    small boxes

--- propermanentmakeup/src/App.js
+++ propermanentmakeup/src/App.js
@@ -2,12 +2,14 @@ import './App.scss';
 import Header from './Components/header.jsx';
 import Footer from './Components/footer.jsx';
 import BigBoxes from './Components/bigBoxes';
+import SmallBoxes from './Components/smallBoxes';
 
 function App() {
   return (
     <>
     <Header />
     <BigBoxes />
+    <SmallBoxes />
     <Footer />
     </>
   );
---
+++ propermanentmakeup/src/Components/smallBoxes.jsx
@@ -0,0 +1,28 @@
+
+
+function SmallBoxes() {
+    return (
+        <div class="row">
+            <div class="col-lg-4">
+                <img src='/images/kreski.jpeg' class="bd-placeholder-img rounded-circle" width="140" height="140" />
+                <h2 class="fw-normal">Kreski</h2>
+                <p>Tę pigmentację pokochała nawet Kleopatra. To klasyczne, ponadczasowe podkreślenie oka.</p>
+                <p><a class="btn btn-secondary" href="#">Pokaż »</a></p>
+            </div>
+            <div class="col-lg-4">
+                <img src='/images/usta.jpeg' class="bd-placeholder-img rounded-circle" width="140" height="140" />
+                <h2 class="fw-normal">Usta</h2>
+                <p>Ustami wyrażamy słowa, uczucia, emocje. Pełne, odpowiednio podkreślone, postrzegane są jako piękne i zmysłowe.</p>
+                <p><a class="btn btn-secondary" href="#">Pokaż »</a></p>
+            </div>
+            <div class="col-lg-4">
+                <img src='/images/brwi.jpeg' class="bd-placeholder-img rounded-circle" width="140" height="140" />
+                <h2 class="fw-normal">Brwi</h2>
+                <p>Brwi to najbardziej wyróżniający się element twarzy. Mówi się, że oczy są zwierciadłem duszy, w takim razie brwi są ramą zwierciadła.</p>
+                <p><a class="btn btn-secondary" href="#">Pokaż »</a></p>
+            </div>
+        </div>
+    );
+}
+
+export default SmallBoxes;
