a4d4ec02e2182cc8542e93fa1f9210453d1e6869
Kamila Kulczyk <kamila.kulczyk@gmail.com>
2023-10-24 13:02:59 +0200

    services

--- propermanentmakeup/src/pages/Services.jsx
+++ propermanentmakeup/src/pages/Services.jsx
@@ -1,107 +1,19 @@
+import Brows from "./services/Brows";
+import Eyelids from "./services/Eyelids";
+import Lips from "./services/Lips";
+import Cover from "./services/Cover";
+import Removal from "./services/Removal";
+import MedicalPigmentation from "./services/MedicalPigmentation";
+
 const Services = () => {
     return(
         <>
-        <hr className="featurette-divider"></hr>
-        <div className="row featurette">
-            <div className="col-md-7">
-                <h2 className="featurette-heading fw-normal lh-1">Brwi 
-                <br/><span className="text-body-secondary">Najbardziej wyróniający się element twarzy</span></h2>
-                <p className="lead">Mówi się, że oczy są zwierciadłem duszy, w takim razie brwi są ramą zwierciadła 🪞 <br/>
-                ▪️ nadają charakter<br/>
-                ▪️ podkreślają oczy <br/>
-                ▪️ akcentują ekspresję<br/>
-                ▪️ mają decydujące znaczenie w wyznaczaniu symetrii twarzy</p>
-                <span className="text-body-secondary">"Detal ma znaczenie"</span>
-            </div>
-            <div className="col-md-5">
-                <img src="/images/portfolio/portfolio8.jpeg" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" title="Makijaż permanentny brwi" alt="Makijaż permanentny brwi" />
-
-            </div>
-        </div>
-        <hr className="featurette-divider"></hr>
-
-        <hr className="featurette-divider"></hr>
-        <div className="row featurette">
-            <div className="col-md-7">
-                <h2 className="featurette-heading fw-normal lh-1">Kreski 
-                <br/><span className="text-body-secondary">Te pigmentację pokochała nawet Kleopatra.
-                </span></h2>
-                <p className="lead">To klasyczne, ponadczasowe podkreślenie oka. <br/>
-                Masz opadająca powiekę? <br/><br/>
-                Delikatna rozcieniowana kreska sprawdzi się doskonale. Moja autorska technika "Kosmiczny Pyłek" dzięki swojej delikatności wygląda dobrze na każdym oku.
-                </p>
-            </div>
-            <div className="col-md-5">
-                <img src="/images/portfolio/portfolio7.jpeg" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" title="Makijaż permanentny brwi" alt="Makijaż permanentny brwi" />
-            </div>
-        </div>
-        <hr className="featurette-divider"></hr>
-
-        <hr className="featurette-divider"></hr>
-        <div className="row featurette">
-            <div className="col-md-7">
-                <h2 className="featurette-heading fw-normal lh-1">Usta 
-                <br/><span className="text-body-secondary">Symbol kobiecości</span></h2>
-                <p className="lead">Ustami wyrażamy słowa, uczucia, emocje.<br/>
-                Pełne, odpowiednio podkreślone, postrzegane są jako piękne i zmysłowe.  Makijaż permanentny podkreśla naturalne piękno, a w efekcie możemy cieszyć się naturalnie wyglądającym makijażem permanentnym, który nigdy się nie rozmazuje<br/><br/>.
-                
-                To jest dla Ciebie jeżeli:<br/>
-                
-                ▪️ Czerwień wargowa z wiekiem zanikła <br/>
-                ▪️ Naturalna czerwień wargowa jest jasna i usta „gubią się” w twarzy <br/>
-                ▪️ Masz blizny na ustach po opryszczkach<br/>
-                ▪️ Masz rozszczep wargi ust<br/>
-                ▪️ Masz asymetrię ust<br/>
-                ▪️ Kontur ust jest niewyraźny <br/>
-                ▪️ Lubisz podkreślać swoje usta</p>
-            </div>
-            <div className="col-md-5">
-                <img src="/images/portfolio/portfolio2.jpeg" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" title="Makijaż permanentny brwi" alt="Makijaż permanentny brwi" />
-            </div>
-        </div>
-        <hr className="featurette-divider"></hr>
-
-        <hr className="featurette-divider"></hr>
-        <div className="row featurette">
-            <div className="col-md-7">
-                <h2 className="featurette-heading fw-normal lh-1">Cover 
-                <br/><span className="text-body-secondary">strona w budowie</span></h2>
-                <p className="lead">strona w budowie</p>
-            </div>
-            <div className="col-md-5">
-                <img src="/images/portfolio/portfolio2.jpeg" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" title="Makijaż permanentny brwi" alt="Makijaż permanentny brwi" />
-
-            </div>
-        </div>
-        <hr className="featurette-divider"></hr>
-
-        <hr className="featurette-divider"></hr>
-        <div className="row featurette">
-            <div className="col-md-7">
-                <h2 className="featurette-heading fw-normal lh-1">Usuwanie makijażu permanentnego i tatuaży 
-                <br/><span className="text-body-secondary">strona w budowie</span></h2>
-                <p className="lead">strona w budowie</p>
-            </div>
-            <div className="col-md-5">
-                <img src="/images/portfolio/portfolio2.jpeg" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" title="Makijaż permanentny brwi" alt="Makijaż permanentny brwi" />
-
-            </div>
-        </div>
-        <hr className="featurette-divider"></hr>
-
-        <hr className="featurette-divider"></hr>
-        <div className="row featurette">
-            <div className="col-md-7">
-                <h2 className="featurette-heading fw-normal lh-1">Pigmentacja medyczna 
-                <br/><span className="text-body-secondary">strona w budowie</span></h2>
-                <p className="lead">strona w budowie</p>
-            </div>
-            <div className="col-md-5">
-                <img src="/images/portfolio/portfolio2.jpeg" className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" title="Makijaż permanentny brwi" alt="Makijaż permanentny brwi" />
-
-            </div>
-        </div>
-        <hr className="featurette-divider"></hr>
+        <Brows />
+        <Eyelids />
+        <Lips />
+        <Cover />
+        <Removal />
+        <MedicalPigmentation />
         </>)
 };
 
